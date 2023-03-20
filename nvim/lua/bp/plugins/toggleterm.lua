cee7486b6680300743697d59a1d56ee9710053fc
Brian <blperry5@gmail.com>
2023-03-20 03:34:07 -0700

    settled on lsp-zero config + formatting in other files

--- nvim/lua/bp/plugins/toggleterm.lua
+++ nvim/lua/bp/plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 return {
     {
-        'akinsho/toggleterm.nvim', 
-        version = "*", 
+        'akinsho/toggleterm.nvim',
+        version = "*",
         config = function()
             require("toggleterm").setup{
             open_mapping = [[<c-\>]]}
